fa8a30975b419c6b751358ee2ba7f28f40c074ed
shahin qoli <shaahin.qoli@gmail.com>
2024-10-19 00:20:59 -0700

    performance in pagination

--- app/finder/spree/products/customized_find.rb
+++ app/finder/spree/products/customized_find.rb
@@ -19,48 +19,12 @@ module Spree
         @store            = params[:store] || Spree::Store.default        
         @taxons           = taxon_ids(params.dig(:filter, :taxons))
         @in_stock         = params.dig(:filter, :in_stock)
-
-        # @ids              = String(params.dig(:filter, :ids)).split(',')
-        # @skus             = String(params.dig(:filter, :skus)).split(',')
-
-        # @price            = map_prices(String(params.dig(:filter, :price)).split(','))
-        # @currency         = current_currency || params.dig(:filter, :currency) || params[:currency]
-        
-        # @concat_taxons    = taxon_ids(params.dig(:filter, :concat_taxons))
-        # @name             = params.dig(:filter, :name)
-        # @options          = params.dig(:filter, :options).try(:to_unsafe_hash)
-        # @option_value_ids = params.dig(:filter, :option_value_ids)
-        # @sort_by          = params.dig(:sort_by)
-        # @deleted          = params.dig(:filter, :show_deleted)
-        # @discontinued     = params.dig(:filter, :show_discontinued)
-        # @properties       = params.dig(:filter, :properties)
-       
-        # @backorderable    = params.dig(:filter, :backorderable)
-        # @purchasable      = params.dig(:filter, :purchasable)
       end
 
       def execute(sort_by,page,per_page)
         product_ids = by_customized(scope)
         product_ids = by_taxons(product_ids)
-        product_ids = ordered(product_ids)
-        # products = show_only_stock(products)
-        
-        # products = by_ids(scope)
-        # products = by_skus(products)
-        # products = by_price(products)
-        # products = by_currency(products)
-        
-        # products = by_concat_taxons(products)
-        # products = by_name(products)
-        # products = by_options(products)
-        # products = by_option_value_ids(products)
-        # products = by_properties(products)
-        # products = include_deleted(products)
-        # products = include_discontinued(products)
-        
-        # products = show_only_backorderable(products)
-        # products = show_only_purchasable(products)
-        # products = ordered(products)
+        product_ids = order_paginate(product_ids,sort_by,page,per_page)
 
         product_ids
       end
