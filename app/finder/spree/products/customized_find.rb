f58e88db11b570497be6d7b25491f064f2760b73
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2024-10-21 13:04:39 +0330

    Update customized_find.rb

--- app/finder/spree/products/customized_find.rb
+++ app/finder/spree/products/customized_find.rb
@@ -2,7 +2,9 @@ module Spree
   module Products
     class CustomizedFind
       def initialize(params:, current_currency: nil)
-
+        @scope ||= Rails.cache.fetch(scope_cache_key, expires_in: 24.hours) do
+          Spree::Taxonomy.first.taxons.map{|item| item.products.map{|item| item.id}}.flatten!.uniq!
+        end
         ActiveSupport::Deprecation.warn('`current_currency` param is deprecated and will be removed in Spree 5') if current_currency
 
         if current_currency.present?
@@ -33,11 +35,6 @@ module Spree
           customized.present?
       end
 
-      def scope
-        @scope ||= Rails.cache.fetch(scope_cache_key, expires_in: 24.hours) do
-          Spree::Taxonomy.first.taxons.map{|item| item.products.map{|item| item.id}}.flatten!.uniq!
-        end
-      end
       
       def taxons?
         taxons.present?
