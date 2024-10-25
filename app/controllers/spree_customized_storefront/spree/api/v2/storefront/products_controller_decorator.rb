a5c19b78f28e924df8703e1578f84602e54e8b0b
shahin qoli <shaahin.qoli@gmail.com>
2024-10-25 13:33:43 -0700

    move to index

--- app/controllers/spree_customized_storefront/spree/api/v2/storefront/products_controller_decorator.rb
+++ app/controllers/spree_customized_storefront/spree/api/v2/storefront/products_controller_decorator.rb
@@ -5,16 +5,20 @@ module SpreeCustomizedStorefront::Spree
       module Storefront
         module ProductsControllerDecorator
 
-          def search
-            @page = params[:page].present? ? params[:page].to_i : 1
-            @per_page = params[:per_page].present? ? params[:per_page].to_i : 24
-            @sort_by = params[:sort_by]
-            base_data = products_data
-            meta = collect_meta_data(base_data, @per_page)
-            links = customized_collection_links(@page)
-            base_data[:meta] = meta
-            base_data[:links] = links
-            render :json => base_data, status: 200
+          def index
+            if params.dig(:filter, :customized).nil?
+              super
+            else
+              @page = params[:page].present? ? params[:page].to_i : 1
+              @per_page = params[:per_page].present? ? params[:per_page].to_i : 24
+              @sort_by = params[:sort_by]
+              base_data = products_data
+              meta = collect_meta_data(base_data, @per_page)
+              links = customized_collection_links(@page)
+              base_data[:meta] = meta
+              base_data[:links] = links
+              render :json => base_data, status: 200
+            end
           end
 
           
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
-Spree::Core::Engine.add_routes do
-  namespace :api, defaults: { format: 'json' } do
-    namespace :v2 do
-      namespace :storefront do
-        resources :products, only: %i[index show] do
-          collection do
-            get "search"
-          end
-        end
-      end  
-    end    
-  end
-end
+# Spree::Core::Engine.add_routes do
+#   namespace :api, defaults: { format: 'json' } do
+#     namespace :v2 do
+#       namespace :storefront do
+#         resources :products, only: %i[index show] do
+#           collection do
+#             get "search"
+#           end
+#         end
+#       end  
+#     end    
+#   end
+# end
